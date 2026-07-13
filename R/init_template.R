ac201c7b5540622aa383569209be08b5900cd6a5
Samuel Bharti <sbharti@uab.edu>
2026-07-12 21:35:38 -0500

    Apply Air formatting to R/

--- R/init_template.R
+++ R/init_template.R
@@ -14,69 +14,69 @@
 #' }
 #' @keywords internal
 init_template <- function(template_name, path = getwd(), confirm = TRUE) {
-
   # Display a message before the prompt
   cat("You current working directory will be:\n")
   cat(path)
 
-  if(confirm){
+  if (confirm) {
     user_input <- tolower(
       readline(
         prompt = "Do you wish to create a project template here? (y/yes to confirm): "
       )
     )
-  }else{
+  } else {
     user_input <- "y"
   }
 
   if (user_input %in% c("y", "yes")) {
+    dest_dir <- path
 
-  dest_dir = path
+    if (template_name == "shiny") {
+      repo_url <- "https://github.com/samuelbharti/RShiny_template"
+      doc_url <- "https://www.samuelbharti.com/posts/r-shiny-template/"
+    } else if (template_name == "cgds") {
+      repo_url <- "https://github.com/uab-cgds-worthey/cgds_repo_template"
+      doc_url <- repo_url
+    }
 
-  if(template_name == "shiny"){
-    repo_url = "https://github.com/samuelbharti/RShiny_template"
-    doc_url = "https://www.samuelbharti.com/posts/r-shiny-template/"
-  }else if(template_name == "cgds"){
-    repo_url = "https://github.com/uab-cgds-worthey/cgds_repo_template"
-    doc_url = repo_url
-  }
+    # Modify the GitHub repo URL to point to the ZIP file
+    zip_url <- paste0(repo_url, "/archive/refs/heads/main.zip")
 
-  # Modify the GitHub repo URL to point to the ZIP file
-  zip_url <- paste0(repo_url, "/archive/refs/heads/main.zip")
+    # Define the path where the ZIP file will be saved
+    temp_dir <- file.path(paste0(dest_dir, "/temp_dir"))
+    dir.create(temp_dir)
 
-  # Define the path where the ZIP file will be saved
-  temp_dir <- file.path(paste0(dest_dir,"/temp_dir"))
-  dir.create(temp_dir)
+    zip_dest <- file.path(temp_dir, "repo.zip") # Save ZIP file in a temporary directory
 
-  zip_dest <- file.path(temp_dir, "repo.zip")  # Save ZIP file in a temporary directory
+    # Download the ZIP file
+    download.file(url = zip_url, destfile = zip_dest, mode = "wb", quiet = TRUE)
 
-  # Download the ZIP file
-  download.file(url =  zip_url, destfile =  zip_dest, mode = "wb", quiet = TRUE)
+    # Extract the ZIP file into a temporary directory
+    unzip(zip_dest, exdir = temp_dir)
 
-  # Extract the ZIP file into a temporary directory
-  unzip(zip_dest, exdir = temp_dir)
+    # Get the name of the unzipped folder (it usually contains "-main" or "-master" at the end)
+    unzipped_folder <- list.dirs(temp_dir, recursive = FALSE)[1] # Assuming the first folder is the repo
 
-  # Get the name of the unzipped folder (it usually contains "-main" or "-master" at the end)
-  unzipped_folder <- list.dirs(temp_dir, recursive = FALSE)[1]  # Assuming the first folder is the repo
+    # Copy the contents from the unzipped folder to the destination directory
+    file.copy(
+      list.files(
+        unzipped_folder,
+        full.names = TRUE,
+        include.dirs = TRUE,
+        all.files = TRUE,
+        no.. = TRUE
+      ),
+      to = dest_dir,
+      recursive = TRUE
+    )
 
-  # Copy the contents from the unzipped folder to the destination directory
-  file.copy(list.files(unzipped_folder,
-                       full.names = TRUE,
-                       include.dirs = TRUE,
-                       all.files = TRUE, no.. = TRUE),
-            to = dest_dir,
-            recursive = TRUE)
+    # Remove the ZIP file after extraction
+    unlink(temp_dir, recursive = TRUE)
 
-  # Remove the ZIP file after extraction
-  unlink(temp_dir, recursive = TRUE)
-
-  cat("Project initialized.\n")
-  cat("Please see documentation at: \n")
-  cat(doc_url)
-
+    cat("Project initialized.\n")
+    cat("Please see documentation at: \n")
+    cat(doc_url)
   } else {
     cat("Project initialization canceled.\n")
   }
-
-
 }
--- R/init_tool_review.R
+++ R/init_tool_review.R
@@ -13,86 +13,75 @@
 #' \dontrun{
 #' tool_review_template("abc","www.abc.com")
 #' }
-tool_review_template <- function(tool_name, tool_url, path = getwd(),
-                                 confirm = TRUE, ...){
-
+tool_review_template <- function(
+  tool_name,
+  tool_url,
+  path = getwd(),
+  confirm = TRUE,
+  ...
+) {
   cat("You current working directory will be:\n")
   cat(path)
 
-  if(confirm){
+  if (confirm) {
     user_input <- tolower(
       readline(
         prompt = "Do you wish to create a project template here? (y/yes to confirm): "
       )
     )
-  }else{
+  } else {
     user_input <- "y"
   }
 
   if (user_input %in% c("y", "yes")) {
-
-
-    if(length(tool_name) != length(tool_url)){
+    if (length(tool_name) != length(tool_url)) {
       print("Please provide URL or Empty qoutes for Each tool")
       return(invisible(NULL))
     }
 
-    base_dir <- c("src","data","notebooks","configs","out","docs")
-    data_dir <- c("shared","preprocessed","other","")
+    base_dir <- c("src", "data", "notebooks", "configs", "out", "docs")
+    data_dir <- c("shared", "preprocessed", "other", "")
 
-
-    script_comment <- function(x_name, x_url){
+    script_comment <- function(x_name, x_url) {
       t_name <- paste0("# Tool name: ", x_name)
       t_url <- paste0("# Tool URL: ", x_url)
-      t_comment <- c("########",
-                     t_name,
-                     t_url,
-                     "########")
+      t_comment <- c("########", t_name, t_url, "########")
       return(t_comment)
     }
 
     wd_path <- path
 
     sapply(base_dir, function(x) {
-
-      aa <- file.path(wd_path,x)
+      aa <- file.path(wd_path, x)
       dir.create(aa, recursive = TRUE, showWarnings = FALSE)
 
-      if(x == "data"){
+      if (x == "data") {
         sapply(data_dir, function(x) {
-
-          bb <- file.path(aa,x)
-          dir.create(bb, recursive = TRUE, showWarnings = FALSE)
-
-          if(x == "preprocessed"){
-            sapply(tool_name, function(x){
-
-              cc <- file.path(bb, x)
-              dir.create(cc, recursive = TRUE, showWarnings = FALSE)
-
-            })
-          }
-
-        })
-      }
-
-      if(x == "out"){
-        sapply(tool_name, function(x){
-
           bb <- file.path(aa, x)
           dir.create(bb, recursive = TRUE, showWarnings = FALSE)
 
+          if (x == "preprocessed") {
+            sapply(tool_name, function(x) {
+              cc <- file.path(bb, x)
+              dir.create(cc, recursive = TRUE, showWarnings = FALSE)
+            })
+          }
         })
-
       }
 
-      if(x == "src"){
-        sapply(1:length(tool_name), function(x){
+      if (x == "out") {
+        sapply(tool_name, function(x) {
+          bb <- file.path(aa, x)
+          dir.create(bb, recursive = TRUE, showWarnings = FALSE)
+        })
+      }
 
-          bb <- file.path(aa, paste0(tool_name[x],".R"))
-          if(file.exists(bb)){
+      if (x == "src") {
+        sapply(1:length(tool_name), function(x) {
+          bb <- file.path(aa, paste0(tool_name[x], ".R"))
+          if (file.exists(bb)) {
             file_info <- file.info(bb)
-            if(is.na(file_info$size) || file_info$size == 0){
+            if (is.na(file_info$size) || file_info$size == 0) {
               # fileConn <- file(bb)
               # writeLines(
               #   script_comment(tool_name[x], tool_url[x]),
@@ -100,10 +89,10 @@ tool_review_template <- function(tool_name, tool_url, path = getwd(),
               # )
               # close(fileConn)
               cat("The file is empty. No changes made.\n")
-            }else {
+            } else {
               cat("The file is not empty. No changes made.\n")
             }
-          }else{
+          } else {
             file.create(bb, showWarnings = FALSE)
             fileConn <- file(bb)
             writeLines(
@@ -119,5 +108,3 @@ tool_review_template <- function(tool_name, tool_url, path = getwd(),
     cat("Project initialization canceled.\n")
   }
 }
-
-
