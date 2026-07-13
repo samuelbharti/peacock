71402a29645bb11c894ea316b324f0bb1622ec66
Samuel Bharti <sbharti@uab.edu>
2026-07-13 01:06:19 -0500

    Only prompt for confirmation in interactive sessions

--- R/init_shiny.R
+++ R/init_shiny.R
@@ -15,7 +15,7 @@ init_shiny <- function(path = getwd(), confirm = TRUE) {
   cat("You current working directory will be:\n")
   cat(path)
 
-  if (confirm) {
+  if (confirm && interactive()) {
     user_input <- tolower(
       readline(
         prompt = "Do you wish to create a project template here? (y/yes to confirm): "
--- R/init_tool_review.R
+++ R/init_tool_review.R
@@ -23,7 +23,7 @@ tool_review_template <- function(
   cat("You current working directory will be:\n")
   cat(path)
 
-  if (confirm) {
+  if (confirm && interactive()) {
     user_input <- tolower(
       readline(
         prompt = "Do you wish to create a project template here? (y/yes to confirm): "
